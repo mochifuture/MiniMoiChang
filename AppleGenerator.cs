cea8c5963507fc5fdbb27b791980395ab9407926
mochi <mochi@mochinoMacBook-puro.local>
2019-03-10 15:55:49 +0900

    Add AppleGeneretor

---
+++ AppleGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleGenerator : MonoBehaviour {
+
+    public GameObject applePrefab;
+    float span = 1.0f;
+    float delta = 0;
+    public bool generateApple = true;
+
+    void Start () {	
+	}
+	
+	void Update () {
+
+        if (this.generateApple == true) {
+            this.delta += Time.deltaTime;
+            if (this.delta > this.span) {
+                this.delta = 0;
+                GameObject apple = Instantiate(applePrefab) as GameObject;
+                float x = Random.Range(-20, 20);
+                float z = Random.Range(-5, 30);
+                apple.transform.position = new Vector3(x, 10, z);
+            }
+        }
+    }
+
+    public void stopGenerateApple() {
+        this.generateApple = false;
+    }
+}
