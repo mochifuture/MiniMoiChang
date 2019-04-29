4046fb32f6ecd45ff6acdbc0efac1ae8f54f56f3
mochi <mochi@mochinoMacBook-puro.local>
2019-04-29 09:48:27 +0900

    Add AppleGeneratorにsetSpanメソッド追加

--- AppleGenerator.cs
+++ AppleGenerator.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class AppleGenerator : MonoBehaviour {
 
     public GameObject applePrefab;
-    float span = 1.0f;
+    float span = 0.3f;
     float delta = 0;
     public bool generateApple = true;
 
-    void Start () {	
-	}
+    void Start () {
+    }
 	
 	void Update () {
 
@@ -21,12 +21,16 @@ public class AppleGenerator : MonoBehaviour {
                 GameObject apple = Instantiate(applePrefab) as GameObject;
                 float x = Random.Range(-20, 20);
                 float z = Random.Range(-5, 30);
-                apple.transform.position = new Vector3(x, 10, z);
+                apple.transform.position = new Vector3(x, 18, z);
             }
         }
     }
 
     public void stopGenerateApple() {
         this.generateApple = false;
+    }
+
+    public void setSpan (float span) {
+        this.span = span;
     }
 }
--- ClearSceneController.cs
+++ ClearSceneController.cs
@@ -6,17 +6,29 @@ using UnityEngine.UI;
 public class ClearSceneController : MonoBehaviour {
 
     MoichangController moichangController;
+    AppleGenerator appleGenerator;
     public GameObject appleCounter;
 
     private int score = 0;
+    private AudioSource soundRingoUmaina;
+    private bool once = true;
 
     void Start() {
         this.moichangController = GameObject.Find("minimoi").GetComponent<MoichangController>();
+        this.appleGenerator = GameObject.Find("AppleGenerator").GetComponent<AppleGenerator>();
         this.appleCounter = GameObject.Find("AppleCounter");
         this.SetCounterValues();
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        this.soundRingoUmaina = audioSources[0];    
     }
 
     void Update() {
+        if(once) {
+            soundRingoUmaina.PlayOneShot(soundRingoUmaina.clip);
+            once = false;
+        }
+        const float span = 0.1f;
+        this.appleGenerator.setSpan(span);
     }
 
     void SetCounterValues() {
